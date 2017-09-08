dff9f247680fe2f38a1508f07e3af6719aa73880
hideokamoto <hide.okamoto@digitalcube.jp>
2017-09-08 19:00:56 +0900

    allow js standard

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-'use strict';
-const Alexa = require('alexa-sdk');
+'use strict'
+const Alexa = require('alexa-sdk')
+const http = require('https')
 
-const SKILL_NAME = 'Shifter Tips';
-const GET_FACT_MESSAGE = "Here's the shifter tips: ";
-const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'See you again!';
+const SKILL_NAME = 'Shifter Tips'
+const GET_FACT_MESSAGE = "Here's the shifter tips: "
+const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?'
+const HELP_REPROMPT = 'What can I help you with?'
+const STOP_MESSAGE = 'See you again!'
 
 const data = [
   'Static web pages are nearly maintenance-free and load at lightening speed. But they require outside development resources and don’t offer the functionality of a dynamic web page.',
@@ -18,42 +19,98 @@ const data = [
   'Pricing starts at $15/mo annually. This is our Personal plan which includes 10GB Storage and 1TB Data Transfer. This covers most users or for more storage and bandwidth we also offer Business and Agency pricing',
   'We also offer a forever trial at no cost for anyone who wants to get started with Shifter today!',
   'Shifter includes global CDN on all accounts for free.',
-  'We host the files that Shifter creates and allow you to connect to custom domains.',
-];
+  'We host the files that Shifter creates and allow you to connect to custom domains.'
+]
 
 const handlers = {
   'LaunchRequest': function () {
-    this.emit('GetNewFactIntent');
+    this.emit('GetNewFactIntent')
   },
   'GetNewFactIntent': function () {
-    const factArr = data;
-    const factIndex = Math.floor(Math.random() * factArr.length);
-    const randomFact = factArr[factIndex];
-    const speechOutput = GET_FACT_MESSAGE + randomFact;
+    const factArr = data
+    const factIndex = Math.floor(Math.random() * factArr.length)
+    const randomFact = factArr[factIndex]
+    const speechOutput = GET_FACT_MESSAGE + randomFact
 
-    this.response.cardRenderer(SKILL_NAME, randomFact);
-    this.response.speak(speechOutput);
-    this.emit(':responseReady');
+    this.response.cardRenderer(SKILL_NAME, randomFact)
+    this.response.speak(speechOutput)
+    this.emit(':responseReady')
   },
-  'AMAZON.HelpIntent': function () {
-    const speechOutput = HELP_MESSAGE;
-    const reprompt = HELP_REPROMPT;
+  'getNewsIntent': function () {
+    httpGet(location, function (response) {
+      // Parse the response into a JSON object ready to be formatted.
+      var responseData = JSON.parse(response)
+      var cardContent = 'Data provided by New York Times\n\n'
 
-    this.response.speak(speechOutput).listen(reprompt);
-    this.emit(':responseReady');
+      // Check if we have correct data, If not create an error speech out to try again.
+      if (responseData == null) {
+        output = 'There was a problem with getting data please try again'
+      } else {
+        output = newsIntroMessage
+
+        // If we have data.
+        for (var i = 0; i < responseData.response.docs.length; i++) {
+          if (i < numberOfResults) {
+            // Get the name and description JSON structure.
+            var headline = responseData.response.docs[i].headline.main
+            var index = i + 1
+
+            output += ' Headline ' + index + ': ' + headline + ';'
+
+            cardContent += ' Headline ' + index + '.\n'
+            cardContent += headline + '.\n\n'
+          }
+        }
+
+        output += ' See your Alexa app for more information.'
+      }
+
+      var cardTitle = location + ' News'
+      alexa.emit(':tellWithCard', output, cardTitle, cardContent)
+    })
+  },
+
+  'AMAZON.HelpIntent': function () {
+    const speechOutput = HELP_MESSAGE
+    const reprompt = HELP_REPROMPT
+
+    this.response.speak(speechOutput).listen(reprompt)
+    this.emit(':responseReady')
   },
   'AMAZON.CancelIntent': function () {
-    this.response.speak(STOP_MESSAGE);
-    this.emit(':responseReady');
+    this.response.speak(STOP_MESSAGE)
+    this.emit(':responseReady')
   },
   'AMAZON.StopIntent': function () {
-    this.response.speak(STOP_MESSAGE);
-    this.emit(':responseReady');
-  },
-};
+    this.response.speak(STOP_MESSAGE)
+    this.emit(':responseReady')
+  }
+}
 
 module.exports.hello = (event, context, callback) => {
-  const alexa = Alexa.handler(event, context, callback);
-  alexa.registerHandlers(handlers);
-  alexa.execute();
-};
+  const alexa = Alexa.handler(event, context, callback)
+  alexa.registerHandlers(handlers)
+  alexa.execute()
+}
+
+function httpGet (query, callback) {
+  console.log('/n QUERY: ' + query)
+
+  url = 'https://getshifter.io/wp-json/wp/v2/posts'
+
+  var req = http.request(url, (res) => {
+    var body = ''
+    res.on('data', (d) => {
+      body += d
+    })
+
+    res.on('end', function () {
+      callback(body)
+    })
+  })
+  req.end()
+
+  req.on('error', (e) => {
+    console.error(e)
+  })
+}
