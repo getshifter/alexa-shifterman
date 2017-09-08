3e6fd22c38a53f3511a6cfeaa3a6224ade211d47
hideokamoto <hide.okamoto@digitalcube.jp>
2017-09-08 14:17:48 +0900

    add simple topic about the shifter

--- index.js
+++ index.js
@@ -8,19 +8,17 @@ const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'See you again!';
 
 const data = [
-  'A year on Mercury is just 88 days long.',
-  'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-  'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-  'On Mars, the Sun appears about half the size as it does on Earth.',
-  'Earth is the only planet not named after a god.',
-  'Jupiter has the shortest day of all the planets.',
-  'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-  'The Sun contains 99.86% of the mass in the Solar System.',
-  'The Sun is an almost perfect sphere.',
-  'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-  'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-  'The temperature inside the Sun can reach 15 million degrees Celsius.',
-  'The Moon is moving approximately 3.8 cm away from our planet every year.',
+  'Static web pages are nearly maintenance-free and load at lightening speed. But they require outside development resources and don’t offer the functionality of a dynamic web page.',
+  'WordPress makes building a website easy and cost effective for almost any user. But it needs regular updates and security can be a concern.',
+  'Generate WordPress to static HTML files with Shifter and deliver them at lightning speed without bottlenecks.',
+  'Create sites that won’t fail or slow down from increased traffic anywhere are in the world.',
+  'SSL is included. Publish in our secure on-demand environment without sweating the security holes or outdated versions of WordPress.',
+  'Maintenance free. End downtime with our unique approach to hosting and publish all your content at once or only when it’s ready.',
+  'Shifter website can deploy to Netlify.',
+  'Pricing starts at $15/mo annually. This is our Personal plan which includes 10GB Storage and 1TB Data Transfer. This covers most users or for more storage and bandwidth we also offer Business and Agency pricing',
+  'We also offer a forever trial at no cost for anyone who wants to get started with Shifter today!',
+  'Shifter includes global CDN on all accounts for free.',
+  'We host the files that Shifter creates and allow you to connect to custom domains.',
 ];
 
 const handlers = {
